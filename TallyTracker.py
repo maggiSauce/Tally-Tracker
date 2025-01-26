844e183ae53c1b2a9b841fcb63c9a109e4384fb8
Ben B <bbui@ualberta.ca>
2025-01-25 17:44:38 -0700

    Did it

--- TallyTracker.py
+++ TallyTracker.py
@@ -1,16 +1,20 @@
+'''
+This is an afternoon project I made because I was too bored to count on my fingers
+'''
+
 from tkinter import *
 import sys
 
 COUNTER = 0
+
 # create Window 
 window = Tk()
 window.title("Tally Tracker")
-
-# set Window size
-window.geometry("480x270")
-
-#scales a window
+window.geometry("250x200")
 window.tk.call('tk', 'scaling', 2.0)
+window.attributes('-topmost', True)
+
+TALLYFONT = ("Arial", 35)
 
 def btnExitClicked():
     window.destroy()
@@ -34,21 +38,18 @@ def updateTally():
     tally.config(text=str(COUNTER))
 
 #  Create a label
-label = Label(window, text='Tally: ')
-label.grid(column=0, row=0)
-
-tally = Label(window, text=COUNTER)
-tally.grid(column=1, row=0)
+tally = Label(window, text=COUNTER, font=TALLYFONT)
+tally.place(x=50,y=0)
 
 upButton = Button(window, text='+', command=btnUpClicked)
-upButton.grid(column=0, row=2)
+upButton.grid(column=0, row=0)
 downButton = Button(window, text='-', command=btnDownClicked)
-downButton.grid(column=0, row=3)
+downButton.grid(column=0, row=1)
 resetButton = Button(window, text='Reset', command=btnResetClicked)
-resetButton.grid(column=1, row=5)
+resetButton.grid(column=1, row=2)
 
 exitButton = Button(window, text='Exit', command = btnExitClicked)
-exitButton.grid(column=2, row=5)
+exitButton.grid(column=2, row=2)
 
 # scanner that scans to see what is happening in the GUI
 window.mainloop()
